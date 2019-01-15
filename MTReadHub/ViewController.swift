73a35519a5a4cc23c321ef40443de278fb337268
MTTGCC <v2top1@163.com>
2019-01-15 10:51:47 +0800

    ignore

--- MTReadHub/ViewController.swift
+++ MTReadHub/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let SRC_WIDTH = UIScreen.main.bounds.width
+    
+    let SRC_HEIGHT = UIScreen.main.bounds.height
+    var tableView:UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setUpTableView() -> UITableView {
+        if tableView == nil {
+             tableView = UITableView(frame: CGRect.init(x: 0, y: 0, width: SRC_WIDTH, height: SRC_HEIGHT), style:UITableView.Style.grouped)
+            
+        }
+        return tableView!
     }
 
 
